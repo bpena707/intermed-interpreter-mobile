2aaf845a7e5d2195faabccbfb3437c7e6b63dc20
bpena707 <46693930+bpena707@users.noreply.github.com>
2024-09-11 21:30:13 -0700

    index page
    the index page has a sign in input and button so user can log in

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,61 @@
-import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
-import { Link } from 'expo-router'
-import {SafeAreaView, Text, View} from 'react-native'
+import { SignedIn, SignedOut, useUser, useSignIn } from '@clerk/clerk-expo'
+import {Link, useRouter} from 'expo-router'
+import {Button, SafeAreaView, Text, TextInput, View} from 'react-native'
+import React, {useState} from "react";
 
 export default function Page() {
     const { user } = useUser()
+    const {signIn, setActive, isLoaded} = useSignIn()
+    const router = useRouter()
+
+    const [emailAddress, setEmailAddress] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onSignInPress = React.useCallback(async () => {
+        if (!isLoaded) {
+            return
+        }
+
+        try {
+            const signInAttempt = await signIn.create({
+                identifier: emailAddress,
+                password,
+            })
+
+            if (signInAttempt.status === 'complete') {
+                await setActive({ session: signInAttempt.createdSessionId })
+                router.replace('/')
+            } else {
+                // See https://clerk.com/docs/custom-flows/error-handling
+                // for more info on error handling
+                console.error(JSON.stringify(signInAttempt, null, 2))
+            }
+        } catch (err: any) {
+            console.error(JSON.stringify(err, null, 2))
+        }
+    }, [isLoaded, emailAddress, password])
 
     return (
         <SafeAreaView>
             <SignedIn>
-                <Text>Hello</Text>
+                <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
             </SignedIn>
             <SignedOut>
-                <Link href="/sign-in">
-                    <Text>Sign In</Text>
-                </Link>
+                <Text>Hello There </Text>
+                <TextInput
+                    autoCapitalize="none"
+                    value={emailAddress}
+                    placeholder="Email..."
+                    onChangeText={(emailAddress) => setEmailAddress(emailAddress)}
+                />
+                <TextInput
+                    value={password}
+                    placeholder="Password..."
+                    secureTextEntry={true}
+                    onChangeText={(password) => setPassword(password)}
+                />
+                <Button title="Sign In" onPress={onSignInPress} />
+
                 <Link href="/sign-up">
                     <Text>Sign Up</Text>
                 </Link>
