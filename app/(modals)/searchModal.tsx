ad52a764c072169b75da1ffc1250ee2209d1382f
bpena707 <46693930+bpena707@users.noreply.github.com>
2025-04-03 20:39:22 -0700

    package changes and search
    breaking change caused rework of npm packages
    
    also started working on the search functionality. backend api and hook and basic modal are rendering but not working yet

--- app/(modals)/searchModal.tsx
+++ app/(modals)/searchModal.tsx
@@ -1,33 +1,18 @@
 import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
+import {Input} from "@/app/components/ui/input";
 
 export default function Tab() {
     return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.modalContainer}>
-                <Text>Lookup patient </Text>
+        <SafeAreaView >
+            <View >
+                <Input
+                    placeholder={'Search patient by name...'}
+                    returnKeyLabel={'Search'}
+                />
             </View>
 
         </SafeAreaView>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(0,0,0,0.5)'
-    },
-    modalContainer: {
-        width: '90%',
-        backgroundColor: 'white', // This is the modal's content background; adjust as needed
-        borderRadius: 10,
-        padding: 20,
-        // Optionally, add a shadow or elevation
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.3,
-        shadowRadius: 4,
-        elevation: 5,
-    },
-});
+
